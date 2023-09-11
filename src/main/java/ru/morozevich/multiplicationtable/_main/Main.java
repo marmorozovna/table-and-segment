cbfa32bb645e88a89c5c8f74b82b7bcbd99f9c44
Марина <marina.morozevich@gmail.com>
2023-09-11 14:28:59 +0300

    formated

--- src/main/java/ru/morozevich/multiplicationtable/_main/Main.java
+++ src/main/java/ru/morozevich/multiplicationtable/_main/Main.java
@@ -8,14 +8,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         MultiplicationTableWriter.printMultiplicationTable();
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Введите координаты начала отрезка по осям x,y:");
         int x1 = sc.nextInt();
         int y1 = sc.nextInt();
         System.out.println("Введите координаты конца отрезка по осям x,y:");
         int x2 = sc.nextInt();
         int y2 = sc.nextInt();
-        double length = SegmentCalculator.calculateSegment(x1,y1,x2,y2);
-        System.out.println("Длина отрезка по заданным координатам: "+length);
+        double length = SegmentCalculator.calculateSegment(x1, y1, x2, y2);
+        System.out.println("Длина отрезка по заданным координатам: " + length);
     }
 }
